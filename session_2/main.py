a897259b95593c7950c422e2d5587cd647994878
BKonstantin <costa.spy@yandex.ru>
2020-09-17 09:11:21 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	sesion 1/nti_base.db

--- session_2/main.py
+++ session_2/main.py
@@ -1,17 +1,14 @@
 from test import Ui_MainWindow
 from PyQt5.QtWidgets import *
 from PyQt5 import QtWidgets, QtGui, QtCore
+import sqlite3
 import time
 
 
 class Loader(QMainWindow, Ui_MainWindow):
     def __init__(self):
-        #super(Loader, self).__init__()
-        #self.setupUi(self)
-
-        super().__init__()
+        super(Loader, self).__init__()
         self.setupUi(self)
-
         self.setWindowTitle('Window 1')
         self.tableWidget.setColumnCount(3)
         self.tableWidget.setRowCount(3)
@@ -20,43 +17,49 @@ class Loader(QMainWindow, Ui_MainWindow):
         self.tableWidget.horizontalHeader().setSectionResizeMode(1, QtWidgets.QHeaderView.Stretch)
         self.tableWidget.horizontalHeader().setSectionResizeMode(2, QtWidgets.QHeaderView.Stretch)
 
-        #self.tableWidget
+        # self.tableWidget
 
         self.spinBox.setValue(3)
         self.spinBox.valueChanged.connect(self.lines)
-        #self.ProgressBar.hide()
+        # self.ProgressBar.hide()
 
         self.pushButton.clicked.connect(self.check)
         self.pushButton_2.clicked.connect(self.write)
 
+        # получаем данные из бд
+        self.con = sqlite3.connect("../nti_base.db")
+        self.cur = self.con.cursor()
+
+        # это названия комплектующих
+        self.list_of_details = []
+        for i in self.cur.execute("""Select title from details""").fetchall():
+            for j in i:
+                self.list_of_details.append(j)
+        self.list_of_details.insert(0, '')
+
+        # это названия акамуляторов
+        self.list_of_akb = []
+        for i in self.cur.execute("""
+                            select title from details where category='Аккумуляторные батареи'""").fetchall():
+            for j in i:
+                self.list_of_akb.append(j)
+
+        #######################
+
         self.n = 3
-
-        self.list = ['АКБ "Сириус-1"', 'АКБ "Пурга-3"', 'АКБ "BreakPower"']
-
-
-
 
         for i in range(3):
             self.add_row(i)
 
+        # self.tableWidget.cellWidget.currentTextChanged.connect(self.ptint)
 
-
-        #self.tableWidget.cellWidget.currentTextChanged.connect(self.ptint)
-
-
-
-        #self.dic = dict()
-
-
-
-
+        # self.dic = dict()
 
     # def go(self):
     #     for i in range(100):
     #         self.ProgressBar.setValue(i)
     #         time.sleep(0.025)
     #     self.ProgressBar.hide()
-
 
     #     self.timer = QtCore.QBasicTimer()
     #     self.timer.start(100, self)
@@ -92,44 +95,31 @@ class Loader(QMainWindow, Ui_MainWindow):
         comboBox = QtWidgets.QComboBox()
         self.tableWidget.setItem(i, 1, QTableWidgetItem())
         self.tableWidget.setItem(i, 2, QTableWidgetItem())
-        #self.tableWidget.closePersistentEditor(self.tableWidget.item(i, 1))
+        # self.tableWidget.closePersistentEditor(self.tableWidget.item(i, 1))
         self.tableWidget.item(i, 1).setFlags(QtCore.Qt.ItemIsEditable)
-        #self.tableWidget.setItem(i, 1, QTableWidgetItem())
+        # self.tableWidget.setItem(i, 1, QTableWidgetItem())
 
-        #self.tableWidget.item(i, 1).setFlags(QtCore.Qt.Item)
+        # self.tableWidget.item(i, 1).setFlags(QtCore.Qt.Item)
 
+        # spinbox = QtWidgets.QSpinBox()
 
-        #spinbox = QtWidgets.QSpinBox()
+        # self.ProgressBar
 
-        #self.ProgressBar
+        # заполняем выпадающее меню
+        comboBox.addItems(self.list_of_details)
+        #################
 
-
-        comboBox.addItems(["",
-                           'АКБ "Сириус-1"',
-                           'Лопасть МК-83 - 100',
-                           'Лопасть Аранж',
-                           'Полетный контроллер  ПК-17-03',
-                           'АКБ "Пурга-3"',
-                           'Регулятор хода РХ-10319',
-                           'Электромотор ДВИЖ-10.50.2019',
-                           'Радиопередатчик "ЭХО-3"',
-                           'Плата распределения питания "ПРП 30.04"',
-                           'Электромотор ВЕТРЯК',
-                           'АКБ "BreakPower"',
-                           'Плата распределения питания "ПРП 20.05"',
-                           ])
         self.tableWidget.setCellWidget(i, 0, comboBox)
         self.tableWidget.cellWidget(i, 0).currentTextChanged.connect(lambda: self.unlock(i))
 
-        #self.tableWidget.setCellWidget(i, 2, spinbox)
+        # self.tableWidget.setCellWidget(i, 2, spinbox)
 
     def unlock(self, i):
-        if self.tableWidget.cellWidget(i, 0).currentText() in self.list:
+        if self.tableWidget.cellWidget(i, 0).currentText() in self.list_of_akb:
             self.tableWidget.setItem(i, 1, QTableWidgetItem())
         else:
             self.tableWidget.setItem(i, 1, QTableWidgetItem())
             self.tableWidget.item(i, 1).setFlags(QtCore.Qt.ItemIsEditable)
-
 
     def add(self):
         self.n += 1
@@ -141,24 +131,29 @@ class Loader(QMainWindow, Ui_MainWindow):
     def check(self):
         # self.ProgressBar.show()
         # self.go()
-        #print(1)
-        #print(self.tableWidget.cellWidget(0, 0).currentText())
-        if self.lineEdit.text():
-            print("Ответсвенный:", self.lineEdit.text())
-        else:
-            return
+        # print(1)
+
+        # Я закоментировал это, потому что оно мешало коду двигаться дальше
+
+        # if self.lineEdit.text():
+        #     print("Ответсвенный:", self.lineEdit.text())
+        # else:
+        #     return
+
+
         for i in range(self.n):
-            if self.tableWidget.cellWidget(i, 0).currentText() in self.list:
+            if self.tableWidget.cellWidget(i, 0).currentText() in self.list_of_akb:
                 print(1)
                 if self.tableWidget.item(i, 1).text():
                     self.tableWidget.item(i, 1).setBackground(QtGui.QColor(255, 255, 255))
                     if self.tableWidget.item(i, 2).text() == "1":
                         self.tableWidget.item(i, 2).setBackground(QtGui.QColor(255, 255, 255))
-                        print(self.tableWidget.cellWidget(i, 0).currentText(), self.tableWidget.item(i, 1).text(), self.tableWidget.item(i, 2).text(), "Штука")
+                        print(self.tableWidget.cellWidget(i, 0).currentText(), self.tableWidget.item(i, 1).text(),
+                              self.tableWidget.item(i, 2).text(), "Штука")
 
 
                     else:
-                        self.tableWidget.item(i, 2).setBackground(QtGui.QColor(255,0,0))
+                        self.tableWidget.item(i, 2).setBackground(QtGui.QColor(255, 0, 0))
                         return
 
                 else:
@@ -169,12 +164,6 @@ class Loader(QMainWindow, Ui_MainWindow):
                 pass
                 # print("not_battery")
         print("all is fine")
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
